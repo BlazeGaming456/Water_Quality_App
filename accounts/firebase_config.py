1fe32356990bc7575a72d5c4bd6ca51fa2affabc
unknown <ajincraju@gmail.com>
2025-06-14 01:09:19 +0530

    adjusted some values

--- accounts/firebase_config.py
+++ accounts/firebase_config.py
@@ -1,13 +1,20 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+from dotenv import load_dotenv
+import os
+load_dotenv()
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+const { api_key } = process.env.fireBaseKey;
+
 const firebaseConfig = {
-  apiKey: "",
+  apiKey: api_key,
   authDomain: "water-quality-analy.firebaseapp.com",
   databaseURL: "https://water-quality-analy-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "water-quality-analy",
