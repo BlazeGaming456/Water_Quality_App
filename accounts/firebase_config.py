ecb1da5dd2da148f2088f505cc420ccb1da1c17b
unknown <ajincraju@gmail.com>
2025-06-14 01:04:00 +0530

    Made some changes

--- accounts/firebase_config.py
+++ accounts/firebase_config.py
@@ -7,7 +7,7 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDXKCr4hBDgFd2nRAN5sr3qWAqGha34Ml0",
+  apiKey: "",
   authDomain: "water-quality-analy.firebaseapp.com",
   databaseURL: "https://water-quality-analy-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "water-quality-analy",
